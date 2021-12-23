9953cd4c73ed71184130d888a0a278ec6b15055d
jin.li <jin.li@aitronx.com>
2021-12-23 02:57:04 -0800

    20211223: fin. chpt. 05 dynamic tf

--- IMU_ws_mk2/LINES354_ws/src/test_pkg/src/demo_07_tf_dynamic_talker_node.cpp
+++ IMU_ws_mk2/LINES354_ws/src/test_pkg/src/demo_07_tf_dynamic_talker_node.cpp
@@ -34,9 +34,12 @@
 
 void doPose(const turtlesim::Pose::ConstPtr &pose) {
     //  5-1.创建 TF 广播器
+
     static tf2_ros::TransformBroadcaster broadcaster;
+
     //  5-2.创建 广播的数据(通过 pose 设置)
     geometry_msgs::TransformStamped tfs;
+
     //  |----头设置
     tfs.header.frame_id = "world";
     tfs.header.stamp = ros::Time::now();
@@ -48,6 +51,7 @@ void doPose(const turtlesim::Pose::ConstPtr &pose) {
     tfs.transform.translation.x = pose->x;
     tfs.transform.translation.y = pose->y;
     tfs.transform.translation.z = 0.0; // 二维实现，pose 中没有z，z 是 0
+
     //  |--------- 四元数设置
     tf2::Quaternion qtn;
     qtn.setRPY(0, 0, pose->theta);
@@ -56,13 +60,13 @@ void doPose(const turtlesim::Pose::ConstPtr &pose) {
     tfs.transform.rotation.z = qtn.getZ();
     tfs.transform.rotation.w = qtn.getW();
 
-
     //  5-3.广播器发布数据
     broadcaster.sendTransform(tfs);
 }
 
 int main(int argc, char *argv[]) {
     setlocale(LC_ALL, "");
+
     // 2.初始化 ROS 节点
     ros::init(argc, argv, "demo_07_dynamic_tf_pub");
 
@@ -71,6 +75,7 @@ int main(int argc, char *argv[]) {
 
     // 4.创建订阅对象
     // 5.回调函数处理订阅到的数据(实现TF广播)
+    // 订阅的 topic：/turtle1/pose 是由 /turtlesim 发布的（龟龟模拟器）
     ros::Subscriber sub = nh.subscribe<turtlesim::Pose>("/turtle1/pose", 1000, doPose);
 
     // 6.spin
